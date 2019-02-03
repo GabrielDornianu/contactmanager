60e4d8c2c3cb2f0699d3662827e1c7e347354929
GabrielDornianu <gabrieldornianu@yahoo.com>
2019-02-03 16:19:09 +0200

    Updates

---
+++ precache-manifest.67439ecd096eb73c8f2baaebc78b20b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "2cac014b9288e275ca1c",
+    "url": "/contactmanager/static/js/main.2cac014b.chunk.js"
+  },
+  {
+    "revision": "801f4fc9dbd4d3cce38b",
+    "url": "/contactmanager/static/js/1.801f4fc9.chunk.js"
+  },
+  {
+    "revision": "2cac014b9288e275ca1c",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "801f4fc9dbd4d3cce38b",
+    "url": "/contactmanager/static/css/1.6b9ab7f6.chunk.css"
+  },
+  {
+    "revision": "cdc878eca95cf6a2fb86bf29a2aa327c",
+    "url": "/contactmanager/index.html"
+  }
+];
